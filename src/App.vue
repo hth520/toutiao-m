8f8120c66108515d16a5fc59c85c2bfb07aaa6e6
feico <1257184579@qq.com>
2022-01-27 13:59:14 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
       <h1>ToDay头条</h1>
+      <div class="toutiao toutiao-shipin"></div>
+
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
